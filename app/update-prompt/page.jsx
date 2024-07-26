5ecb0ebf9dec40991f70b6deb6bcc11f9834c2fb
Timishot <timidotcom58@gmail.com>
2024-07-26 13:38:31 +0100

    fix

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,64 +1,12 @@
-"use client";
+import React, { Suspense } from 'react';
+import EditPrompt from '@components/EditPrompt';
 
-import { useEffect, useState } from "react";
-import { useRouter, useSearchParams } from "next/navigation";
-import Form from "@components/Form";
-
-const EditPrompt = () => {
-  const router = useRouter();
-  const [searchParams] = useSearchParams();
-  const promptId = searchParams.get("id");
-
-  const [submitting, setIsSubmitting] = useState(false);
-  const [post, setPost] = useState({ prompt: "", tag: "" });
-
-  useEffect(() => {
-    const getPromptDetails = async () => {
-      if (promptId) {
-        try {
-          const response = await fetch(`/api/prompt/${promptId}`);
-          const data = await response.json();
-          setPost({ prompt: data.prompt, tag: data.tag });
-        } catch (error) {
-          console.error(error);
-        }
-      }
-    };
-
-    getPromptDetails();
-  }, [promptId]);
-
-  const updatePrompt = async (e) => {
-    e.preventDefault();
-    setIsSubmitting(true);
-
-    if (!promptId) return alert("Prompt not found");
-
-    try {
-      const response = await fetch(`/api/prompt/${promptId}`, {
-        method: "PATCH",
-        body: JSON.stringify({ prompt: post.prompt, tag: post.tag }),
-      });
-
-      if (response.ok) {
-        router.push("/");
-      }
-    } catch (error) {
-      console.error(error);
-    } finally {
-      setIsSubmitting(false);
-    }
-  };
-
+function UpdatePromptPage() {
   return (
-    <Form
-      type="Edit"
-      post={post}
-      setPost={setPost}
-      submitting={submitting}
-      handleSubmit={updatePrompt}
-    />
+    <Suspense fallback={<div>Loading...</div>}>
+      <EditPrompt />
+    </Suspense>
   );
-};
+}
 
-export default EditPrompt;
+export default UpdatePromptPage;
