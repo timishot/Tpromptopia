e5b3476eba343f4043c9d8e5ce638d1b0ba8d34d
Timishot <timidotcom58@gmail.com>
2024-07-26 12:52:57 +0100

    fix

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,9 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
-
 import { useRouter, useSearchParams } from "next/navigation";
-
 import Form from "@components/Form";
 
 const EditPrompt = () => {
@@ -13,44 +11,40 @@ const EditPrompt = () => {
 
   const [submitting, setIsSubmitting] = useState(false);
   const [post, setPost] = useState({ prompt: "", tag: "" });
-  console.log(promptId)
+
   useEffect(() => {
     const getPromptDetails = async () => {
-        const response = await fetch(`/api/prompt/${promptId}`);
-        const data = await response.json();
-        console.log(data);
-        setPost(
-          {
-            prompt: data.prompt,
-            tag: data.tag,
-          }
-        )
-    }
+      if (promptId) {
+        try {
+          const response = await fetch(`/api/prompt/${promptId}`);
+          const data = await response.json();
+          setPost({ prompt: data.prompt, tag: data.tag });
+        } catch (error) {
+          console.error(error);
+        }
+      }
+    };
 
-    if (promptId) {
-      getPromptDetails();
-    }
-  },[promptId])
+    getPromptDetails();
+  }, [promptId]);
 
   const updatePrompt = async (e) => {
     e.preventDefault();
     setIsSubmitting(true);
 
     if (!promptId) return alert("Prompt not found");
+
     try {
       const response = await fetch(`/api/prompt/${promptId}`, {
         method: "PATCH",
-        body: JSON.stringify({
-          prompt: post.prompt,
-          tag: post.tag,
-        }),
+        body: JSON.stringify({ prompt: post.prompt, tag: post.tag }),
       });
 
       if (response.ok) {
         router.push("/");
       }
     } catch (error) {
-      console.log(error);
+      console.error(error);
     } finally {
       setIsSubmitting(false);
     }
@@ -58,7 +52,7 @@ const EditPrompt = () => {
 
   return (
     <Form
-      type='Edit'
+      type="Edit"
       post={post}
       setPost={setPost}
       submitting={submitting}
