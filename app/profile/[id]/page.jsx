fcadf9cd80f13f7a913643a79d88a8c11f3604fc
Timishot <timidotcom58@gmail.com>
2024-07-26 12:45:11 +0100

    push

--- app/profile/[id]/page.jsx
+++ app/profile/[id]/page.jsx
@@ -6,7 +6,7 @@ import { useSearchParams } from "next/navigation";
 import Profile from "@components/Profile";
 
 const UserProfile = ({ params }) => {
-  const searchParams = useSearchParams();
+  const [searchParams] = useSearchParams();
   const userName = searchParams.get("name");
 
   const [userPosts, setUserPosts] = useState([]);
